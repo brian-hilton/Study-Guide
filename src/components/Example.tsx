73c18791020f9b5fd9bc5fa58acb9b39303f32e9
brian-hilton <brian.hilton67@gmail.com>
2025-02-27 21:52:03 -0600

    add component

---
+++ src/components/Example.tsx
@@ -0,0 +1,15 @@
+function Example() {
+  return (
+    <>
+      <div className="study-card">
+        <h2>Typescript</h2>
+        <ul>
+          <li>Superset of Javascript</li>
+          <li>Statically Typed</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Example;
