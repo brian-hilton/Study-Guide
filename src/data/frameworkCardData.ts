1844808518e700879b7629459ceae7b59bc6617a
brian-hilton <brian.hilton67@gmail.com>
2025-03-03 22:08:26 -0600

    adding some data stuff

---
+++ src/data/frameworkCardData.ts
@@ -0,0 +1,10 @@
+export const frameworkCardData = [
+    {
+        title: "Python",
+        webFrameworks: ["Django, Flask, CherryPy"]
+    },
+    {
+        title: "C#",
+        webFrameworks: ["Asp.net Core"]
+    }
+];
