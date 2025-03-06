77d77b487587a939f801144110d16be817ead774
brian-hilton <brian.hilton67@gmail.com>
2025-03-05 21:03:24 -0600

    fix frameworkCard component

--- src/components/FrameworkCard.tsx
+++ src/components/FrameworkCard.tsx
@@ -4,13 +4,21 @@ import "../styles/frameworkCard.css";
 
 interface frameworkCardProps {
   title: string;
-  frameworks: { [key: string]: string };
+  frameworks: { [key: string]: string[] };
 }
 
 function FrameworkCard({ title, frameworks }: frameworkCardProps) {
   return (
     <>
       <div className="framework-card"></div>
+      <h3>{title}</h3>
+      <ul>
+        {Object.entries(frameworks).map(([category, list]) => (
+          <li key={category}>
+            <strong>{category}:</strong> {list.join(", ")}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
